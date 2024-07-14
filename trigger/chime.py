6b8f21e9061d65d9cf27781c95b6f7100106d063
poduck <poduck@gmail.com>
2024-07-13 21:58:00 -0400

    Added pushbullet notification

--- trigger/chime.py
+++ trigger/chime.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import requests
 from time import sleep
+from pushbullet import Pushbullet
+
+pb = Pushbullet('o.YdKuyOyPo9xJKgqzT09a2N8l82roHwjT')
 
 sensor_pin = 21
 
@@ -13,6 +16,8 @@ try:
     while True:
         if GPIO.input(sensor_pin) and play_clip:
             r = requests.get('http://localhost/clips/trigger/')
+            dev = pb.get_device('Samsung SM-N975U')
+            push = dev.push_note("Alert!!", "Someone came in the door")
             sleep(3)
         if not GPIO.input(sensor_pin):
             play_clip = True
