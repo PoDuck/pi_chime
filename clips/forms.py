9eedebb0966513880e34f9bc3c063fc9e7d3b79e
poduck <poduck@gmail.com>
2024-07-21 13:21:28 -0400

    Added slider to volume

--- clips/forms.py
+++ clips/forms.py
@@ -2,7 +2,18 @@ from django import forms
 from .models import Clip
 
 
+class RangeInput(forms.TextInput):
+    template_name = "clips/widgets/range.html"
+
+
 class ClipUploadForm(forms.ModelForm):
     class Meta:
         model = Clip
         fields = ["title", "game", "thumbnail", "file", "max_volume", "start_time", "end_time"]
+        widgets = {'max_volume': RangeInput(
+            attrs={
+                'type': 'range',
+                'oninput': 'this.nextElementSibling.value = this.value',
+                'class': 'form-range w-25'
+            }
+        )}
