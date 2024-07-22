65d2ca22a65556bb1bbf23f5ba7a99e0d711eb4e
poduck <poduck@gmail.com>
2024-07-22 11:01:21 -0400

    Enable host from .env file.

--- trigger/chime.py
+++ trigger/chime.py
@@ -2,8 +2,20 @@ import RPi.GPIO as GPIO
 import requests
 from time import sleep
 from pushbullet import Pushbullet
+import environ
+from pathlib import Path
+import os
 
-pb = Pushbullet('o.YdKuyOyPo9xJKgqzT09a2N8l82roHwjT')
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+pb = env('PUSHBULLET_KEY')
 
 sensor_pin = 21
 
@@ -15,7 +27,7 @@ play_clip = True
 try:
     while True:
         if GPIO.input(sensor_pin) and play_clip:
-            r = requests.get('http://localhost/clips/trigger/')
+            r = requests.get('http://' + env('LOCAL_DOMAIN') + env('LOCAL_PORT') + '/clips/trigger/')
             dev = pb.get_device('Samsung SM-N975U')
             push = dev.push_note("Alert!!", "Someone came in the door")
             sleep(3)
