e62987279954942855b3ba6868e208ce5e7bf655
poduck <poduck@gmail.com>
2024-07-23 10:20:34 -0400

    Navbar changes

--- chime/settings.py
+++ chime/settings.py
@@ -129,11 +129,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = str(BASE_DIR / 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-#     # This defines a prefix so the url paths will become `/static/node_modules/...`
-#     ('node_modules', os.path.join(BASE_DIR, 'node_modules/')),
-# )
+STATICFILES_DIRS = (
+    ('chime_static', os.path.join(BASE_DIR, 'chime_static/')),
+)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = str(BASE_DIR / 'media')
